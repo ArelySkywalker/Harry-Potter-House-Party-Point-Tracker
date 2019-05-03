0e1ce762d31d6dd104b9940528a59874abcdf2a7
James Boullion <jboullion83@gmail.com>
2019-05-02 21:43:49 -0500

    Setup Basic House Component

---
+++ src/components/Houses.vue
@@ -0,0 +1,66 @@
+<template>
+	<b-container fluid>
+		<b-card-group deck>
+			<House v-for="house in houses" :house="house" :key="house.name"></House>
+		</b-card-group>
+	</b-container>
+</template>
+
+<script>
+import House from "./Houses/House.vue";
+
+export default {
+	data () {
+		return {
+			houses: [
+				{
+					"name": "Gryffindor",
+					"colors": {
+						"primary" : "#7F0909",
+						"secondary" : "#FFC500"
+					},
+					"mascot": "Griffin",
+					"gif": "https://github.com/ArelySkywalker/HarryPotterHouseParty/blob/master/images/gryffindor.gif?raw=true"
+				},
+				{
+					"name": "Ravenclaw",
+					"colors": {
+						"primary" : "#000A90",
+						"secondary" : "#946B2D"
+					},
+					"mascot": "Raven",
+					"gif": "https://github.com/ArelySkywalker/HarryPotterHouseParty/blob/master/images/ravenclaw.gif?raw=true"
+				},
+				{
+					"name": "Slytherin",
+					"colors": {
+						"primary" : "#0D6217",
+						"secondary" : "#AAAAAA"
+					},
+					"mascot": "Serpent",
+					"gif": "https://github.com/ArelySkywalker/HarryPotterHouseParty/blob/master/images/slytherin.gif?raw=true"
+				},
+				{
+					"name": "Hufflepuff",
+					"colors": {
+						"primary" : "#EEE117",
+						"secondary" : "#000000"
+					},
+					"mascot": "Badger",
+					"gif": "https://github.com/ArelySkywalker/HarryPotterHouseParty/blob/master/images/hufflepuff.gif?raw=true"
+				}
+			]
+			
+		}
+	},
+	components: {
+		House
+	}
+}
+</script>
+
+<style>
+
+
+
+</style>
---
+++ src/components/Houses/House.vue
@@ -0,0 +1,54 @@
+<template>
+	<b-card 
+		bg-variant="dark"
+		header="Dark"
+		text-variant="white"
+		class="text-center"
+		no-body
+	>
+		<h4 slot="header">{{house.name}}</h4>
+
+		<b-card-img :src="house.gif" :alt="house.name" top></b-card-img>
+		<b-card-body>
+			<b-card-title>{{Math.floor(Math.random() * 100)}} Points</b-card-title>
+			<b-card-text>{{numPlayers}} Members</b-card-text>
+
+			 <ul class="list-unstyled text-left mt-5">
+				<b-media tag="li" v-for="index in numPlayers" :key="index" class="mb-1">
+					<b-img slot="aside" blank blank-color="#abc" width="64" alt="placeholder"></b-img>
+
+					<h5 class="mt-0 mb-1">{{index}}. {{randomName()}}</h5>
+					<p>{{Math.floor(Math.random() * 10)}} points</p>
+				</b-media>
+			</ul>
+		</b-card-body>
+
+	</b-card>
+</template>
+
+<script>
+
+
+export default {
+	props: ['house'],
+	data () {
+		return {
+			numPlayers: Math.floor(Math.random() * 10) + 1
+		}
+	},
+	methods:{
+		randomName(){
+			return Math.random().toString(36).substring((Math.floor(Math.random() * 10)));
+		}
+	},
+	components: {
+		
+	}
+}
+</script>
+
+<style>
+
+
+
+</style>
